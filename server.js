84e89ecc29c98fbdb17a7a85917466e276d7f136
Abhay Doke <abhaydoke09@gmail.com>
2017-02-21 01:44:23 -0500

    Demo

--- server.js
+++ server.js
@@ -9,4 +9,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
-app.listen(3000);
+//app.listen(5000);
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
